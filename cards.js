7159852b43def36c82c309f9163fd1d45b162269
Tamo <tamo@meilisearch.com>
2024-05-27 23:04:46 +0200

    start adding js

---
+++ cards.js
@@ -0,0 +1,7 @@
+export default cards = [
+  {
+    "notion": "Psychopathologie",
+    "definition": "Troubles mentaux et l'étude qui s'y rattache. Cette discipline tente de comprendre, d'expliquer et de remédier à la maladie psychique.",
+    "label": ["psycho", "math", "definition"],
+  }
+];
