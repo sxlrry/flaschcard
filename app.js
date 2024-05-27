083704f3a33ff103291a1b61fed8079612774df6
Tamo <tamo@meilisearch.com>
2024-05-27 23:41:50 +0200

    make the card generic

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-import cards from "cards";
+import cards from "./cards.js";
+
+console.log(cards[0]);
+
+function generate_card_html(card) {
+  return `<div class="flip-card-inner">
+        <div class="flip-card-front">
+              <h1 class="Notion"> Notion </h1>
+              <p class="Question"> ${card.question} </p>
+        </div>
+        <div class="flip-card-back">
+              <h1 class="Définition"> Définition </h1>
+              <p class="Réponse"> ${card.reponse} </p>
+        </div>
+  </div>
+  <div class="barre">
+        <button class="bouton1"> je le sais  </button>
+        <button class="bouton2"> à moitié  </button>
+        <button class="bouton3"> pas du tout  </button>
+  </div>`
+}
+
+document.getElementsByClassName("flip-card")[0].innerHTML = generate_card_html(cards[0]);
--- cards.js
+++ cards.js
@@ -1,7 +1,9 @@
-export default cards = [
+const cards = [
   {
-    "notion": "Psychopathologie",
-    "definition": "Troubles mentaux et l'étude qui s'y rattache. Cette discipline tente de comprendre, d'expliquer et de remédier à la maladie psychique.",
+    "question": "Psychopathologie",
+    "reponse": "Troubles mentaux et l'étude qui s'y rattache. Cette discipline tente de comprendre, d'expliquer et de remédier à la maladie psychique.",
     "label": ["psycho", "math", "definition"],
   }
 ];
+
+export default cards;
